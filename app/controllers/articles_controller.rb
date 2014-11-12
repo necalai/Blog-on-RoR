2a762e86bda94163ae78f7fe6c51d2e9a08b8732
OlayNick <OlayNick@users.noreply.github.com>
2014-11-12 14:08:27 +0300

    Revert "Revert "Fixed edit posts"" Create migration addArtAuthor This reverts commit c467f0e13582847f4724ae756df077b9025dcd70.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.author = current_user.username
     
     if @article.save
       redirect_to @article
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109172331) do
+ActiveRecord::Schema.define(version: 20141112083617) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author"
   end
 
   create_table "comments", force: true do |t|
