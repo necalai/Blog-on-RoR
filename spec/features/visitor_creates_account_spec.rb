f0af8a48a64993c25eaa5023fc2ddf1ce37b3176
Nick Olay <vast87@mail.ru>
2014-11-30 20:38:26 +0400

    Fixed account_spec

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -7,13 +7,3 @@ feature "Account Creation" do
   end
 end
 
-def sing_up
-  visit new_user_registration_path
-
-      fill_in :user_email, :with => 'user@example.com'
-      fill_in :user_username, :with => 'mike'
-      fill_in :user_password, :with => 'secure123!@#'
-      fill_in :user_password_confirmation, :with => 'secure123!@#'
-
-      click_button 'Sign up'
-end
