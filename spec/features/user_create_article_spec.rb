f2cb8e23b8c05a0a900615b1abbbb3a8f7e3c274
Nick Olay <vast87@mail.ru>
2014-11-30 20:37:15 +0400

    Add spec for article

---
+++ spec/features/user_create_article_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+feature "Article Creation" do 
+  before(:all) do
+    sing_up
+  end
+  scenario "allows user to visit new article page" do      
+    visit new_article_path
+    expect(page).to have_content 'New article'
+  end
+end
+
+
