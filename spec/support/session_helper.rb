61ac21cd0698dea632b8c7806b3985be22503151
Nick Olay <vast87@mail.ru>
2014-11-30 20:10:12 +0400

    Refactoring move sing_up to session_helper.rb

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,12 +1,3 @@
-require "spec_helper"
-
-feature "Account Creation" do 
-  scenario "allows guest to create account" do      
-    sing_up
-    expect(page).to have_content I18n.t('devise.registrations.signed_up')
-  end
-end
-
 def sing_up
   visit new_user_registration_path
 
