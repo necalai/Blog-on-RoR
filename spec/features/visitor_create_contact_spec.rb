444c1f7280a434f6d469bbcf80336843eae5ae78
Nick Olay <vast87@mail.ru>
2014-11-30 18:13:56 +0400

    Add create contact acceptance tests

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 feature "Contact Creation" do 
   scenario "allows acees to contacts page" do
-      visit '/contacts/sdf'
+      visit '/contacts'
 
       expect(page).to have_content 'Contact us'
   end   
